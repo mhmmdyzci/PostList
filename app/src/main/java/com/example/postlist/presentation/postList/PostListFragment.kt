a26f2b47aa3fa11ad72c7a23d30825b4357c4088
muhammedyazici <191805001@stu.adu.edu.tr>
2026-09-08 19:03:52 +0300

    Create SplashScreen

---
+++ app/src/main/java/com/example/postlist/presentation/postList/PostListFragment.kt
@@ -0,0 +1,15 @@
+package com.example.postlist.presentation.postList
+
+
+import com.example.postlist.databinding.FragmentPostListBinding
+import com.example.postlist.presentation.base.BaseFragment
+
+
+class PostListFragment : BaseFragment<FragmentPostListBinding>(FragmentPostListBinding::inflate) {
+
+
+
+
+
+
+}
